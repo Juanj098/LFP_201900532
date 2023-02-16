fea69066f65d201535d566a61797fc231339fb38
Juan Gerardi <juanjgerardiher1998@gmail.com>
2023-02-16 00:07:23 -0600

    15/02

--- index.py
+++ index.py
@@ -1,1 +1,26 @@
-print("hello world")
+print('----------Bienvenido-------------')
+Doc= input("Ingrese documento ->")
+
+with open(Doc, 'r') as file:
+    contenido = file.readlines()
+
+peliculas=[]
+class Movies:
+    def __init__(self,name,actors,year,gender):
+        self.name = name
+        self.actors = actors
+        self.year = year
+        self.gender = gender
+    def mostrar(self):
+        print(self.name,self.actors,self.year,self.gender)
+    def MostrarActors(self):
+        pass        
+
+for linea in contenido:
+    datos = linea.split(";")
+    pelicula = Movies(datos[0],datos[1],datos[2],datos[3])
+    peliculas.append(pelicula)
+for i  in range(len(peliculas)):
+    print([i],peliculas[i].mostrar())
+
+
