b40e9543d1b3b4c98aa09b0eb6bf5a297c60029d
Juan Gerardi <juanjgerardiher1998@gmail.com>
2023-02-16 23:57:36 -0600

    modificado

--- index.py
+++ index.py
@@ -1,10 +1,9 @@
-print('----------Bienvenido-------------')
-Doc= input("Ingrese documento ->")
+print('<----------Bienvenido------------->')
+Doc = input("Ingrese documento -> ")
 
 with open(Doc, 'r') as file:
     contenido = file.readlines()
 
-peliculas=[]
 class Movies:
     def __init__(self,name,actors,year,gender):
         self.name = name
@@ -13,14 +12,31 @@ class Movies:
         self.gender = gender
     def mostrar(self):
         print(self.name,self.actors,self.year,self.gender)
-    def MostrarActors(self):
-        pass        
+    def mostrarActors(self):
+        print(self.actors)      
 
+print("<--------------------------------->")
+print("<             MENU                >")
+print("1. Gestionar peliculas")
+print("2. Filtrado")
+print("3. Graficar")
+print("4. salir")
+print("<--------------------------------->")
+opc = input("Ingrese opcion: ")
+peliculas=[]
 for linea in contenido:
     datos = linea.split(";")
     pelicula = Movies(datos[0],datos[1],datos[2],datos[3])
     peliculas.append(pelicula)
-for i  in range(len(peliculas)):
-    print([i],peliculas[i].mostrar())
-
-
+if opc == "1":
+    for i  in range(len(peliculas)):
+        if peliculas[i].mostrar() != None:
+            print(peliculas[i].mostrar())
+elif opc == "2":
+    print("Holi :3")
+elif opc == "3":
+    print("opcion 3")
+elif opc == "4":
+    print("opcion 4")
+else:
+    print("opcion invalida rata inutil >:p ")
